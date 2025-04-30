fc095ba8ac63318a64b02c0cbcc3e713fd57eb6b
Lewise55 <157831388+Lewise55@users.noreply.github.com>
2025-04-30 04:38:53 +0000

    added, favorites, characters, and planets id + links favorites

--- src/models.py
+++ src/models.py
@@ -1,12 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, Boolean, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    login: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
@@ -14,6 +14,56 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "login": self.login,
             # do not serialize the password, its a security breach
+        }
+    
+class Favorites(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    # user id (who favorited)--relate to user table
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    # character id (if favored, store id)--relate to character table
+    character_id: Mapped[int] = mapped_column(ForeignKey("characters.id"))
+    # planet id (if favored, store id)--relate to planet table
+    planet_id: Mapped[int] = mapped_column(ForeignKey("planets.id"))
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "character_id": self.character_id,
+            "planet_id": self.planet_id,
+        }
+
+class Characters(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    eye_color: Mapped[str] = mapped_column(String(120), nullable=False)
+    Hair_color: Mapped[str] = mapped_column(String(120), nullable=False)
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "eye_color": self.eye_color,
+            "hair_color": self.hair_color
+        }
+
+class Planets(db.Model):
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    climate: Mapped[str] = mapped_column(String(120), nullable=False)
+    terrain: Mapped[str] = mapped_column(String(120), nullable=False)
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "climate": self.climate,
+            "terrain": self.terrain
         }
